b77218466287a73f360031a7df1b79c9c5b14bd3
RaphaelcliffsGarcia <cliffsdesigner@gmail.com>
2024-12-07 18:46:37 -0300

    sprite + game + main

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # BomberMan
-
-Teste
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from assets import Assets
+from game import Game
 import gamesettings as gs
 
 
@@ -9,23 +11,28 @@ class BomberMan:
         self.screen = pygame.display.set_mode((gs.SCREENWIDTH, gs.SCREENHEIGHT))
         pygame.display.set_caption("BomberMan")
 
+        self.ASSETS = Assets()
+        self.GAME = Game(self, self.ASSETS)
         self.FPS = pygame.time.Clock()
 
         self.run = True
 
 
     def input(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.run = False
+        #for event in pygame.event.get():
+        #    if event.type == pygame.QUIT:
+        #        self.run = False
+        self.GAME.input()
 
 
     def update(self):
         self.FPS.tick(gs.FPS)
+        self.GAME.update()
 
 
     def draw(self, window):
         window.fill(gs.BLACK)
+        self.GAME.draw(window)
         pygame.display.update()
 
 
